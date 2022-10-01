38b04d19ac5658031989f3e83769be2259d2523c
Kristina <kristisha_m@mail.ru>
2022-10-02 00:01:42 +0300

    add task2 in new console project

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число М:");
+int numberM = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine()!);
+
+int SumFromMToN(int numberM, int numberN)
+{
+    int sum=0;
+    if (numberM == numberN)
+    {
+        return numberM;
+    }
+    sum += numberN;
+    return SumFromMToN(numberM, numberN - 1) + sum;
+}
+Console.WriteLine($"Сумма чисел от {numberM} до {numberN} = {SumFromMToN(numberM, numberN)}");
